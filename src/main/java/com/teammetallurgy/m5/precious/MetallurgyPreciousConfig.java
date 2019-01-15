a7d20c76d7a264138fe4e4df96fe222d73f964b4
Keith Manning <RebelKeithy@gmail.com>
2019-01-14 23:17:53 -0600

    Fixed error in Precious submod

--- src/main/java/com/teammetallurgy/m5/precious/MetallurgyPreciousConfig.java
+++ src/main/java/com/teammetallurgy/m5/precious/MetallurgyPreciousConfig.java
@@ -6,15 +6,4 @@ import com.teammetallurgy.m5.core.utils.MetallurgyConfig;
 
 public class MetallurgyPreciousConfig extends MetallurgyConfig {
     
-    public static MetalDefinition[] metals = { };
-    
-    public static void load(String configPath) {
-        for(MetalDefinition metal : metals)
-            loadConfig(configPath, metal);
-    }
-    
-    public static void register() {
-        for(MetalDefinition metal : metals)
-            MetalRegistry.registerMetal(metal);
-    }
 }
